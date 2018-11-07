7568ea35018937fcd97fff39bbcd4c61669b81c2
fsmytsai <msphausung1998@gmail.com>
2018-11-07 09:41:03 +0800

    Update by fsmytsai

--- app/Services/ChatService.php
+++ app/Services/ChatService.php
@@ -47,7 +47,7 @@ class ChatService
         $keyword = '%' . $chatData['keyword'] . '%';
 
         $DataList = DB::table('chats')->where('chats.chat_name', 'like', $keyword)
-            ->select('chats.chat_id', 'chats.chat_name', 'chats.creator', 'chats.profile_pic as chat_profile_pic', 'user.name as creator_name', 'user.profile_pic as creator_profile_pic', 'chat_members.status')
+            ->select('chats.chat_id', 'chats.chat_name', 'chats.created_at', 'chats.creator', 'chats.profile_pic as chat_profile_pic', 'user.name as creator_name', 'user.profile_pic as creator_profile_pic', 'chat_members.status')
             ->join('user', 'chats.creator', '=', 'user.account')
             ->leftJoin('chat_members', function ($join) use ($chatData) {
                 $join->on('chat_members.chat_id', '=', 'chats.chat_id')
@@ -55,6 +55,11 @@ class ChatService
             })
             ->orderBy('chats.chat_id', 'desc')
             ->get();
+        $baseService = new BaseService();
+        foreach ($DataList as $item) {
+            $timeDistance = $baseService->timeDistance($item->created_at);
+            $item->created_at = $timeDistance;
+        }
 
         return $DataList;
     }
