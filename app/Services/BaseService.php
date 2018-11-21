91c915f3b9a84a24317aa2d2bc9640bee32aff72
fsmytsai <msphausung1998@gmail.com>
2018-11-21 13:58:34 +0800

    Update by fsmytsai

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 
 class BaseService
 {
-    function setAllTime(&$dataList)
+    function setAllTime($dataList)
     {
         foreach ($dataList as $item) {
             if ($item->created_at) {
@@ -20,7 +20,6 @@ class BaseService
     {
         $now = Carbon::now();
         $carbonTime = Carbon::parse($time);
-//        $distance = strtotime($now) - strtotime($time);
 
         if ($carbonTime->hour < 12) {
             if ($carbonTime->hour == 0)
@@ -40,29 +39,4 @@ class BaseService
 
         return $resStr;
     }
-
-
-//    public function json2String($jsonData)
-//    {
-//        return json_encode($jsonData, JSON_FORCE_OBJECT);
-//    }
-
-
-//        if ($Distance < 60) {
-//            return $Distance . '秒前';
-//        } else if ($Distance < 3600) {
-//            return floor($Distance / 60) . '分鐘前';
-//        } else if ($Distance < 86400) {
-//            return floor($Distance / 3600) . '小時前';
-//        } else if ($Distance < 5184000) {
-//            return floor($Distance / 86400) . '天前';
-//        } else {
-//            $startTimeArr = explode($delimiter, $time->toDateTimeString());
-//            $endTimeArr = explode($delimiter, now());
-//            if ($endTimeArr[0] - $startTimeArr[0] > 0) {
-//                return $endTimeArr[0] - $startTimeArr[0] . '年前';
-//            } else {
-//                return $endTimeArr[1] - $startTimeArr[1] . '個月前';
-//            }
-//    }
 }
