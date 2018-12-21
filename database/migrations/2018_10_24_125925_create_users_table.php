9e1e0cb368fbbc041ba9bf0bd2ac22c0aaa45d46
fsmytsai <msphausung1998@gmail.com>
2018-12-21 14:44:16 +0800

    Update by fsmytsai

--- database/migrations/2018_10_24_125925_create_users_table.php
+++ database/migrations/2018_10_24_125925_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
