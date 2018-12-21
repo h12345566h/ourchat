32cb9d8e76d729d2e8b059abbb2205b16a551ddb
fsmytsai <msphausung1998@gmail.com>
2018-12-21 14:43:33 +0800

    Update by fsmytsai

--- database/migrations/2018_10_24_125925_create_users_table.php
+++ database/migrations/2018_10_24_125925_create_users_table.php
@@ -13,7 +13,7 @@ class CreateUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->string('account', 20)->primary();
             $table->string('password', 60);
             $table->string('name', 20);
@@ -30,6 +30,6 @@ class CreateUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('users');
     }
 }
